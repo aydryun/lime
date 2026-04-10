1ad8b8e5fce8514891c7baa34e22b647aa8d2bf5
aydryun <42096409+aydryun@users.noreply.github.com.>
2026-04-10 14:57:59 +0200

    feat: du systeeme de theme ajout du selecteur de theme & shadcn

---
+++ chat-client/components/theme-selector.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+  const themes = ["light", "dark", "system", "lime"];
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {themes.map((theme) => {
+          return (
+            <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
+              {theme.charAt(0).toUpperCase() + theme.slice(1)}
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
