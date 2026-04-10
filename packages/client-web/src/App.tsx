4a1974002a7c90647d41f1fc2386346f9118b761
root <root@IdeaPadSlim3.localdomain>
2026-04-10 10:03:53 +0200

    clean du repo

--- packages/client-web/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export function App() {
-  return (
-    <div>
-      <h1>Lime</h1>
-      <p>Client web</p>
-    </div>
-  );
-}
--- packages/server-core/src/routes/documents.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-export const documents = new Hono();
-
-// DELETE /api/documents/:id
-documents.delete("/:id", (c) => {
-  // TODO: supprimer le document en BDD
-  return c.json({ message: "Document supprimé" });
-});
