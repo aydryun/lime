d724e3f3b0f0453665be188fe293822edf1dc318
aydryun <42096409+aydryun@users.noreply.github.com.>
2026-04-23 14:30:43 +0200

    feat: Implementation de l'interfance frontend

--- chat-client/components/theme-selector.tsx
+++ chat-client/components/theme-selector.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { Citrus, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -19,8 +19,9 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 lime:scale-0 lime:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 lime:scale-0 lime:rotate-90" />
+          <Citrus className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all lime:scale-100 lime:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
